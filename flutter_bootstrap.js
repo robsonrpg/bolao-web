e330fb16ddf2a35d93447c7e2b3c97bfba62299b
Robson de Melo Silva <robson@MacBook-Air-de-Lunna.local>
2026-06-10 23:12:17 -0300

    Deploy web 2026-06-10_23-12-17

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1376133207" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3587129050" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
